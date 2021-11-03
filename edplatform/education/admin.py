493f80105d44907bded9892d5852c799cdfe37bf
boredom101 <ymnewmark@gmail.com>
2021-11-03 12:26:21 +0200

    added first version of admin for testing

--- edplatform/education/admin.py
+++ edplatform/education/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.EducationLevel)
+admin.site.register(models.Grade)
+admin.site.register(models.Subject)
+admin.site.register(models.Subtopic)
