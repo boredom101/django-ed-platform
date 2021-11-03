afbae71e5996385ef8fda51270b53864d2e23e4e
boredom101 <ymnewmark@gmail.com>
2021-11-03 11:24:30 +0200

    added all views aside for json view

---
+++ edplatform/education/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('grades/list/', views.GradeListView.as_view(), name="grades"),
+    path('subjects/list/', views.SubjectListView.as_view(), name="subjects"),
+    path('subjects/create/', views.SubjectCreateView.as_view(), name="subject-create"),
+    path('subjects/<int:pk>', views.SubjectDetailView.as_view(), name="subject-detail"),
+    path('subtopics/list/', views.SubtopicListView.as_view(), name="subtopics"),
+    path('subtopics/create/', views.SubtopicCreateView.as_view(), name="subtopic-create"),
+    path('subtopics/<int:pk>', views.SubtopicDetailView.as_view(), name="subtopic-detail"),
+]
--- edplatform/education/views.py
+++ edplatform/education/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from . import models
+
+class GradeListView(generic.ListView):
+    model = models.Grade
+
+class SubjectListView(generic.ListView):
+    model = models.Subject
+
+class SubtopicListView(generic.ListView):
+    model = models.Subtopic
+
+class SubjectDetailView(generic.DetailView):
+    model = models.Subject
+
+class SubtopicDetailView(generic.DetailView):
+    model = models.Subtopic
+
+class SubjectCreateView(generic.CreateView):
+    model = models.Subject
+
+class SubtopicCreateView(generic.CreateView):
+    model = models.Subtopic
